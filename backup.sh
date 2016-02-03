1dd9275c037adbe2e46a8d1be247b59a053278db
SEAP UDEA <seapudea@gmail.com>
2016-02-03 07:30:41 -0500

    Commit

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash          
+database="Sinfin"
+backdir="data/dump"
+datadir="data/recon"
+
+echo "Dumping..."
+mysqldump -u root -p $database > $backdir/$database.sql
+echo "Compressing..."
+tar cf $backdir/$database.tar $backdir/$database.sql $datadir
+p7zip $backdir/$database.tar
+echo "Git adding..."
+git add --all -f $backdir/*.tar.*
+echo "Done."
