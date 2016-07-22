3eee91090ce3951aa6cff8aa729c1739c1e64dc5
Astronomia UdeA <astronomia.udea@gmail.com>
2016-07-22 06:31:13 -0500

    Commit

--- backup.sh
+++ backup.sh
@@ -5,9 +5,17 @@ datadir="data/recon data/movilidad"
 
 echo "Dumping..."
 mysqldump -u root -p $database > $backdir/$database.sql
+
 echo "Compressing..."
 tar cf $backdir/$database.tar $backdir/$database.sql $datadir
 p7zip $backdir/$database.tar
+
+echo "Splitting..."
+cd $backdir
+rm $database.tar.7z-*
+split -b 1024k $database.tar.7z $database.tar.7z-
+cd - &> /dev/null
+
 echo "Git adding..."
-git add --all -f $backdir/*.tar.*
+git add --all -f $backdir/*.tar.7z-*
 echo "Done."
