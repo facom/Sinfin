e565054f9e8b271d26a92bf965f61b8a11575553
SEAP UDEA <seapudea@gmail.com>
2017-04-26 20:03:56 -0500

    Commit

---
+++ test-site/Programas_files/template.js
@@ -0,0 +1,65 @@
+
+jQuery(function($){
+    $("#toggle-menu").on('click',function(){
+        $(this).next().slideToggle();
+    });
+    $('._sub_menu > img').remove();
+    $('._sub_menu > .image-title').addClass('icon-home');
+    $('.acordeon').on('click','h2',function(){
+        var t = $(this);
+        var td = t.next();
+        var d = t.parent().siblings().find('div');
+        td.slideToggle()
+        d.slideUp()
+
+    });
+
+    var $header = $('#header'), $menu = $('#menu');
+    var $aH = $header.height();
+    $("#clr").css("height",$aH);
+    $header.removeClass('fixed');
+    $(window).on('scroll', function(){
+        var scroll = $(this).scrollTop();
+        if (scroll > $aH) {
+            $header.addClass('fixed');
+            $menu.css({'width':'auto','margin-top':'1em'});
+        }else{
+            $header.removeClass('fixed');
+            $menu.css({'width':'100%','margin-top':'0'});
+        }
+    })
+});
+
+function movimiento(index,l){
+    if(l === 1){
+        if(index < 7){
+            $('.evento_contenedor .borde:nth-child('+index+')').addClass('none').removeClass('block');
+            index = index+3;
+            $('.evento_contenedor .borde:nth-child('+(index)+')').addClass('block').removeClass('none');
+        }
+    }else{
+        if(index > 0){
+            index = index+3;
+            $('.evento_contenedor .borde:nth-child('+index+')').addClass('none').removeClass('block');
+            index = (-index+6)+1;
+            $('.evento_contenedor .borde:nth-child('+index+')').addClass('block').removeClass('none');
+
+        }
+    }
+}
+
+function mover(index, obj){
+
+    if(index == 0){
+        obj.css('left',0);
+    } else if(index > 0){
+        obj.css('left','-'+100*index+'%');
+    }
+}
+
+//script de boton de busqueda
+
+
+$(document).ready(function(){
+});
+
---
+++ test-site/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" class="no-js" xmlns="http://www.w3.org/1999/xhtml" xml:lang="" lang="" slick-uniqueid="3"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--<base href="http://extension.medicinaudea.co/index.php/programas">--><base href=".">
+    <meta property="og:url" content="http://fcen-udea.co/principal/Sinfin/index.php">
+    <meta property="og:title" content="SInfIn">
+    <meta property="og:type" content="website">
+
+    <title>Programas</title>
+
+    <link rel="stylesheet" href="./Programas_files/k2.fonts.css" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/k2.css" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/style.css" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/calendar-jos.css" type="text/css" title="Verde" media="all">
+    <link rel="stylesheet" href="./Programas_files/settings.css" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/dynamic-captions.css" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/static-captions.css" type="text/css">
+
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link rel="stylesheet" href="./Programas_files/normalize.css">
+    <link rel="stylesheet" href="./Programas_files/template.css">
+    <link href="./Programas_files/css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="./Programas_files/custom.css">
+    <link href="./Programas_files/icons.css" rel="stylesheet">
+
+  </head>
+
+<body>
+
+<header id="header" class="fixed">
+  <br>
+
+  <div class="grupo movil-tabla"> 
+
+    <div class="caja movil-50 izquierda-contenido">
+      <div class="logo">
+        <a href="http://extension.medicinaudea.co/index.php"><img src="./Programas_files/logofacultaddemed.svg"></a>
+      </div>
+    </div>
+
+    <div class="caja movil-50 derecha-contenido redes_sociales">
+      <div class="custom">
+	<div class="redes_ext">
+	  <a href="http://www.facebook.com/cemedicinaudea/" target="_blank" class="icon" title="Facebook"></a>
+	  <a href="https://twitter.com/medicina_udea" target="_blank" class="icon" title="Twitter"></a>
+	  <a href="http://www.youtube.com/channel/UCs1_F_FhSLJ_z9UJycxK3UQ" target="_blank" class="icon" title="Youtube"></a>
+	  <a href="mailto:cemedicina@udea.edu.co" target="_blank" class="icon" title="Correo"></a>
+	</div>
+      </div>
+      
+      <div class="info_sitio">
+	<p>Educación Continua</p>
+      </div>  
+    </div>
+  </div>
+
+  <div class="grupo">
+      <div class="caja web-100 movil-100"> 
+        
+	<div id="toggle-menu" class="icon-align-justify centrar-texto">Menú Principal</div>
+        <div class="menu_ppal">
+          <ul class="nav ">
+	    <li class="item-200"><a href="http://extension.medicinaudea.co/index.php">Inicio</a></li>
+	    <li class="item-256"><a href="http://extension.medicinaudea.co/index.php/quienes-somos">Quiénes somos</a></li>
+	    <li class="item-257 current active parent"><a href="http://extension.medicinaudea.co/index.php/programas">Programas</a></li>
+	    <li class="item-299"><a href="http://extension.medicinaudea.co/index.php/servicios">Servicios</a></li>
+	    <li class="item-259"><a href="http://extension.medicinaudea.co/index.php/educacion-a-la-medida">Educación a la medida</a></li>
+	    <li class="item-260"><a href="http://extension.medicinaudea.co/index.php/videos">Vídeos</a></li>
+	    <li class="item-261"><a href="http://extension.medicinaudea.co/index.php/certificados">Certificados</a></li>
+	    <li class="item-258"><a href="http://extension.medicinaudea.co/index.php/contacto">Contacto</a></li>
+	  </ul>
+        </div>
+      </div>
+  </div>
+</header>
+
+<section id="componente">
+  <div class="grupo">
+    <div class="caja tablet-100">
+      <div id="clr"></div>
+      <br/>
+      Curso de evaluación económica de 36 horas, dictado por el profesor
+      Aurelio Mejía Mejía, Economista, MSc en Economía de la Salud. El
+      curso tendrá como objetivo conocer y aplicar las principales
+      técnicas de la evaluación económica en salud y comprender su
+      aporte en el proceso de toma de decisiones en salud.
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div>
+</section>
+  
+<div class="grupo">
+  <div class="caja total">
+  </div>
+</div>
+
+<div class="grupo">
+  <div class="caja total">
+  </div>
+</div>
+<div id="modulos_extras">
+    <div class="grupo">
+        <div class="caja total">
+            
+        </div>
+    </div>
+</div>
+
+<footer id="footer">
+  <section id="footer_top">
+    <div class="grupo tablet-tabla">
+      <div class="caja tablet-25">
+	<div class="custom">
+	  <h5>Oriéntese</h5>
+	  <ul>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/" target="_blank">Atención al ciudadano</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/canales-atencion/" target="_blank">Canales de atencion al ciudadano</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/contdirectoriotelefonico/" target="_blank">Directorio telefónico</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/notificaciones-judicales" target="_blank">Notificaciones judiciales</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/contpreguntasfrecuentes/" target="_blank">Preguntas frecuentes
+	    </a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/normativa" target="_blank">Normativa
+	    </a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/transparencia/" target="_blank">Transparencia y acceso a la información</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/atencion-ciudadano/atencion-ciudadano/contenido/ascontenidosmenu/transparencia/" target="_blank">Mapa del sitio</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/somos-udea/empleados/seguridad-personas-bienes/autorizaciones/contenido/asmenulateral/ingreso-salida-vehiculos/!ut/p/z1/1VVLc5swEP4rzsFHRsvDQI8EmxmnNnEdu8ZcMjIIWy1IBIFb59dXpGknTmo87uiQcGK1q293v30IxShCMcN7usU15QznUl7H9r37yTd0z4IJhOMheLa59BfjCQTXFlp1GQCYKD6tHvlO9_3WQXsfTnweoK8oRnHC6rLeoXXJqxrnTUpwH7A4lna8IH_-BS-4-C2QoswJTrnogyDbpqIpTnu4V5JKSAJE79DbUMKIVOOm5hV9xIlkppUTzmrCaMpbxIKwJsc1qXDeB8q2FRFcEziXcNqe7GjS5Fy0oZYJTdHaxQkMMqlLs42tWUmGNTcxHM1wbSC6bW9M5ww1LXfxZaX5rA_A-zKahYtbfza8Mc6XJu4yCYdOt8FTiF0GT-1xbOCCG8ggF5ORtZwC3A7eILzN4lyLrCUPzskg70x0d2FhugENQzWgrhpQdcpjUA2omsOxag69y1O-OTcsctnSbw8PsSc3WrtcftYoeu8rTcZsVFN_upVU4HqnUZZxFHX6RdGxXxT99StVL_2i6KTfzuoEc8X9E8wVj0zgWKoBddWAqlMObbUj075x_26__-injzh7ZbFcFq55oNr3-Wjugjko94-LrCjuw1DDm-eDCVl5s1dn0mg1PPzwrq5-AUfoKqU!/dz/d5/L2dBISEvZ0FBIS9nQSEh/?urile=wcm%3Apath%3A%2FPortalUdeA%2FasPortalUdeA%2FasHomeUdeA%2FasSomosUdeA%2FEmpleados%2FSeguridad%20a%20personas%20y%20bienes%2FAutorizaciones%2FContenido%2FasMenuLateral%2Fingreso-salida-vehiculos" target="_blank">Pico y placa en Ciudad Universitaria</a></li>
+	  </ul>
+	</div>
+
+      </div>
+
+      <div class="caja tablet-25">
+	<div class="custom">
+	  <h5>Servicios en línea
+	  </h5>
+	  <ul>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/sistema-bibliotecas" target="_blank">Peticiones, quejas, reclamos y sugerencias</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/cultura/museo-universitario" target="_blank">Registro de usuarios externos</a></li>
+	    <li><a href="http://www.parquedelemprendimiento.com/" target="_blank">Iniciar sesión del Portal</a></li>
+	    <li><a href="http://aprendeenlinea.udea.edu.co/" target="_blank">Consultar correo electrónico</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/extension-UdeA/practicas" target="_blank">Aspirantes a docente de cátedra
+
+	    </a></li>
+	</ul></div>
+
+      </div>
+      <div class="caja tablet-25">
+        
+
+	<div class="custom">
+	  <h5>Proyectos estratégicos</h5>
+	  <ul>
+	    <ul>
+	      <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/sistema-bibliotecas" target="_blank">Sistema de Bibliotecas</a></li>
+	      <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/cultura/museo-universitario" target="_blank">Museo universitario</a></li>
+	      <li><a href="http://www.parquedelemprendimiento.com/" target="_blank">Parque E</a></li>
+	      <li><a href="http://aprendeenlinea.udea.edu.co/" target="_blank">Aprende en línea</a></li>
+	      <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/extension-UdeA/practicas" target="_blank">Prácticas</a></li>
+	      <li><a href="http://www.escuelagobierno.org/escuela/" target="_blank">Escuela de gobierno y Políticas Públicas de Antioquia
+	      </a></li>
+	      <li><a href="http://parquedelavida.com/" target="_blank">Parque de la vida</a></li>
+	      <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/institucional/donaciones" target="_blank">Fondo de donaciones</a></li>
+	      <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/saluda" target="_blank">Programa de salud</a></li>
+	</ul></ul></div>
+
+      </div>
+
+      <div class="caja tablet-25">
+	<div class="custom">
+	  <h5>Estudiar en la UdeA
+	  </h5>
+	  <ul>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/estudiar-udea/pregrado/oferta/" target="_blank">Pregrado</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/estudiar-udea/posgrado/oferta/" target="_blank">Posgrado</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/estudiar-udea/udea-educacion-virtual/" target="_blank">Ude@ Educación virtual</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/estudiar-udea/educacion-continua" target="_blank">Formación continua</a></li>
+	    <li><a href="http://www.udea.edu.co/wps/portal/udea/web/inicio/estudiar-udea/otras-opciones" target="_blank">Otras opciones
+
+	    </a></li>
+	</ul></div>
+
+      </div>
+    </div>
+  </section>
+  <section id="footer_bottom">
+    <div class="grupo tablet-tabla">
+      <div class="caja total">
+        
+      </div>
+    </div>
+  </section>
+</footer>
+<section id="licencias">
+  <div class="grupo">
+    <div class="caja centrar-contenido">
+
+      <div class="custom">
+	<div class="caja tablet-100">
+	  <div class="homecopyright" dir="ltr">
+	    <div class="homecopyright-texto" style="text-align: center;"><img src="./Programas_files/logo.png" alt="logo"></div>
+	    <div>&nbsp;</div>
+	    <div class="homecopyright-texto" style="text-align: center;">Dirección: Clle 67 No. 53-108<br>
+	      <div class="homecopyright-texto" style="text-align: center;">Conmutador: [57+4] 219 6940<br> <a href="http://extension.medicinaudea.co/cemedicina@udea.edu.co">vicedecacen@udea.edu.co</a> <br> Medellín - Colombia | Todos los Derechos Reservados © 2017</div>
+	    </div>
+	  </div>
+	</div>
+
+      </div>
+      
+    </div>
+  </div>
+</section>
+
+<script src="./Programas_files/jquery.min(1).js"></script>
+<script type="text/javascript" src="./Programas_files/template.js"></script>
+
+<style id="service-icons-0"></style> 
+</body></html>
